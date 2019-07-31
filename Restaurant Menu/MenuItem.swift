2771309fe18c41c4813dd9d403aa2e2e2c66933f
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-07-31 12:06:24 +0300

    Created MenuItem model file

---
+++ Restaurant Menu/MenuItem.swift
@@ -0,0 +1,31 @@
+//
+//  MenuItem.swift
+//  Restaurant Menu
+//
+//  Created by Rinalds Domanovs on 31/07/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import Foundation
+
+struct MenuItem: Codable {
+    var id: Int
+    var name: String
+    var detailText: String
+    var price: Double
+    var category: String
+    var imageURL: URL
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case detailText = "description"
+        case price
+        case category
+        case imageURL = "image_url"
+    }
+}
+
+struct MenuItems: Codable {
+    let items: [MenuItem]
+}
