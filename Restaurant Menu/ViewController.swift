65f6e3487443efa4d2ba2557a0d02b118e43dc7e
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-07-31 09:48:58 +0300

    Initial Commit

---
+++ Restaurant Menu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant Menu
+//
+//  Created by Rinalds Domanovs on 31/07/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
