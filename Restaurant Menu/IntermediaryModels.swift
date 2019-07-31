f84e5095b46338a23f38bd5c93d0c283e357f1e3
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-07-31 12:08:00 +0300

    Created Categories model in IntermediaryModels file

---
+++ Restaurant Menu/IntermediaryModels.swift
@@ -0,0 +1,13 @@
+//
+//  IntermediaryModels.swift
+//  Restaurant Menu
+//
+//  Created by Rinalds Domanovs on 31/07/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import Foundation
+
+struct Categories: Codable {
+    let categories: [String]
+}
