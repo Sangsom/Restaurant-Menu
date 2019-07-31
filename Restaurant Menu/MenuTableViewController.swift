f7f8b5652f1424097efeb892144541100c14bed2
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-07-31 14:53:41 +0300

    Fetching data for menuItems

--- Restaurant Menu/MenuTableViewController.swift
+++ Restaurant Menu/MenuTableViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class MenuTableViewController: UITableViewController {
 
+    let menuController = MenuController()
+    var menuItems = [MenuItem]()
     var category: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        title = category.capitalized
+        menuController.fetchMenuItems(forCategory: category) { (menuItems) in
+            if let menuItems = menuItems {
+                self.updateUI(with: menuItems)
+            }
+        }
     }
 
+    func updateUI(with menuItems: [MenuItem]) {
+        DispatchQueue.main.async {
+            self.menuItems = menuItems
+            self.tableView.reloadData()
+        }
+    }
 }
